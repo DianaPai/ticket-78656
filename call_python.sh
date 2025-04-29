9a15f62d260335e2eb87af3b7bc59a23e97579eb
integration-test integration-test <test-notifs+integration-test@dominodatalab.com>
2025-04-29 10:56:51 -0700

    Pushed from Domino: https://sup601gh75734.support-team-sandbox.domino.tech/workspace/integration-test/ticket78868?executionId=680965ccc37fb73335698940

--- call_python.sh
+++ call_python.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #python git_commit_push.py -m "commit from shell script"
-python add_comment_and_commit.py helloworld.py -c "Added logging for debug" -m "Add comment for logging" -b main
+#python add_comment_and_commit.py helloworld.py -c "Added logging for debug" -m "Add comment for logging" -b main
+python /mnt/imported/code/ticket-78656/add_comment_and_commit.py /mnt/imported/code/ticket-78656/helloworld.py -c "Added logging for debug" -m "Add comment for logging" -b main
 
